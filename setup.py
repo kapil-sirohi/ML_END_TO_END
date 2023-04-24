1afaf05d44bd2ea4baac596f68ebd0ddc143ca07
Kapil Sirohi <kapilsirohi25@gmail.com>
2023-04-24 09:21:32 +0530

    Added setup and requirements file and updated README.md

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup,find_packages
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements.
+
+    '''
+    HYPHEN_E_DOT = '-e .'
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n', '') for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    return requirements
+        
+
+
+setup(
+
+name = 'ML_Project',
+version='0.0.1',
+author='Kapil',
+author_email='Kapilsirohi25@gmail.com',
+install_requires=get_requirements('requirements.txt'),
+packages=find_packages()
+
+
+
+)
