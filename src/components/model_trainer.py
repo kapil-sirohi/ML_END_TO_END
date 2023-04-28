263469a68e64deb3b5f6dd1983121c85dfbbcaa6
Kapil Sirohi <kapilsirohi25@gmail.com>
2023-04-28 10:53:06 +0530

    Added app.py and templates, updated model_trainer.py, predict_pipeline.py and utils.py

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -16,7 +16,7 @@ from sklearn.neighbors import KNeighborsRegressor
 
 from src.exception import CustomException
 from src.logger import logging
-from src.utils import save_object, evaluate_model
+from src.utils import save_object, evaluate_models
 
 
 @dataclass
@@ -32,7 +32,7 @@ class ModelTrainer:
     def initiate_model_trainer(self, train_array, test_array):
         try:
             logging.info("splitting training and test array")
-            logging.info(train_array)
+            #logging.info(train_array)
             X_train,y_train,X_test,y_test=(
                 train_array[:,:-1],
                 train_array[:,-1],
@@ -41,20 +41,56 @@ class ModelTrainer:
             )
 
             models = {
-
-                "Random Forest" : RandomForestRegressor(),
-                "Decision Tree" : DecisionTreeRegressor(),
-                "Gradient Boosting" : GradientBoostingRegressor(),
-                "Linear Regression" : LinearRegression(),
-                "K-Neighbour Regressor": KNeighborsRegressor(),
-                "XGBoost Regressor" : XGBRegressor(),
-                "CatBoost Regressor" : CatBoostRegressor(verbose=False),
-                "Adaboost Regressor" : AdaBoostRegressor()
-
+                "Random Forest": RandomForestRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
+                "Linear Regression": LinearRegression(),
+                "XGBRegressor": XGBRegressor(),
+                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor": AdaBoostRegressor(),
             }
 
-            model_report:dict=evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
-                                             models=models)
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+
+
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
+                                             models=models,param=params)
 
             best_model_score = max(sorted(model_report.values()))
 
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import pickle
 import dill
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 
@@ -22,24 +23,41 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_model(X_train, y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
+
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            model.fit(X_train, y_train)  # train model
+            para=param[list(models.keys())[i]]
 
-            y_train_predict = model.predict(X_train)
-            y_test_predict = model.predict(X_test)
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
-            train_model_score = r2_score(y_train,y_train_predict)
-            test_model_score = r2_score(y_test,y_test_predict)
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
 
             report[list(models.keys())[i]] = test_model_score
 
         return report
 
-
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
 
     except Exception as e:
-        raise  CustomException(e,sys)
+        raise CustomException(e, sys)
